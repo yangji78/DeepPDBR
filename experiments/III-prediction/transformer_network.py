0c2d5b04a3cd07150e5c7d6751ab2f2bff87ed56
yangji78 <781359447@qq.com>
2024-07-15 13:42:41 +0800

    update...

--- experiments/III-prediction/transformer_network.py
+++ experiments/III-prediction/transformer_network.py
@@ -2,19 +2,6 @@ import torch
 import torch.nn as nn
 import math
 
-class PositionalEncoding(nn.Module):
-    def __init__(self, d_model, max_len=5000):
-        super(PositionalEncoding, self).__init__()
-        self.encoding = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
-        self.encoding[:, 0::2] = torch.sin(position * div_term)
-        self.encoding[:, 1::2] = torch.cos(position * div_term)
-        self.encoding = self.encoding.unsqueeze(0)  # 添加 batch 维度
-
-    def forward(self, x):
-        return x + self.encoding[:, :x.size(1), :]
-
 class LearnedPositionalEncoding(nn.Module):
     def __init__(self, d_model, max_len=103):
         super(LearnedPositionalEncoding, self).__init__()
